e08e0a253d9fddf3dc34d13adc6bf14b4a0b829c
Matt McCormick <matt.mccormick@kitware.com>
2023-03-10 17:15:13 -0500

    ENH: Add TypeScript sources
    These can be modified as desired.
    
    Addition to the repository will support publication of docs and demos on
    Netlify.

---
+++ wasm/typescript/src/itk-cleaver-options.ts
@@ -0,0 +1,24 @@
+import { Image } from 'itk-wasm'
+
+interface ItkCleaverOptions {
+  /** Input label image or multiple indicator function images */
+  input?: Image
+
+  /** Blending function sigma for input(s) to remove alias artifacts. */
+  sigma?: number
+
+  /** Sizing field sampling rate. The default sample rate will be the dimensions of the volume. Smaller sampling creates coarser meshes. */
+  samplingRate?: number
+
+  /** Sizing field rate of change. the maximum rate of change of element size throughout a mesh. */
+  lipschitz?: number
+
+  /** Sizing field feature scaling. Scales features of the mesh effecting element size. Higher feature scaling creates coaser meshes. */
+  featureScaling?: number
+
+  /** Sizing field padding. Adds a volume buffer around the data. Useful when volumes intersect near the boundary. */
+  padding?: number
+
+}
+
+export default ItkCleaverOptions
