9cc174dbc6fb2eddd82109b11d5bc892a62f23d9
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-04-28 14:52:49 -0400

    DOC: Fix license referenced in setup.py
    The license associated with the package is now consistent with the one
    of the Cleaver project.

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ setup(
     description=r'An ITK interface to the Cleaver multi-material tetrahedral meshing library',
     long_description='Cleaver2 ( https://github.com/SCIInstitute/Cleaver2/releases) is a free multimaterial tetrahedral meshing tool developed by the NIH Center for Integrative Biomedical Computing at the University of Utah Scientific Computing and Imaging (SCI) Institute.',
     classifiers=[
-        "License :: OSI Approved :: Apache Software License",
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python",
         "Programming Language :: C++",
         "Development Status :: 4 - Beta",
@@ -39,7 +39,7 @@ setup(
         "Operating System :: Unix",
         "Operating System :: MacOS"
         ],
-    license='Apache',
+    license='MIT',
     keywords='ITK InsightToolkit',
     url=r'https://itk.org/',
     install_requires=[
