721901c61eb91ba0ef8c44a5fa86b520ad153465
Hans Johnson <hans-johnson@uiowa.edu>
2025-01-26 09:16:06 -0600

    STYLE: Rename ITK_DISALLOW_COPY_AND_ASSIGN to ITK_DISALLOW_COPY_AND_MOVE
    Clarifies that the macro does not just disallow copy and assign, but
    also move operations. Note that in this context, the term 'move' refers
    to both move-construct and move-assign.
    
    With this commit, the old macro name will remain available, as long as
    'ITK_FUTURE_LEGACY_REMOVE = OFF' (which is the default).

--- include/itkCleaverImageToMeshFilter.h
+++ include/itkCleaverImageToMeshFilter.h
@@ -37,7 +37,7 @@ template <typename TInputImage, typename TOutputMesh>
 class CleaverImageToMeshFilter : public ImageToMeshFilter<TInputImage, TOutputMesh>
 {
 public:
-  ITK_DISALLOW_COPY_AND_ASSIGN(CleaverImageToMeshFilter);
+  ITK_DISALLOW_COPY_AND_MOVE(CleaverImageToMeshFilter);
 
   static constexpr unsigned int InputImageDimension = TInputImage::ImageDimension;
   static constexpr unsigned int OutputMeshDimension = TOutputMesh::PointDimension;
